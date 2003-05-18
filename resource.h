ac743f7014b76699002556b0b531b5b136477c36
Jeff Lawson <jeff@bovine.net>
2003-05-18 21:32:17 +0000

    remove unused id.  mark resource file for non-MFC project

--- resource.h
+++ resource.h
@@ -3,7 +3,6 @@
 // Used by stuntour.rc
 //
 #define IDD_CONFIRM                     101
-#define IDC_CHECK1                      1001
 #define IDC_CHECKREMEMBER               1001
 #define IDC_SERVERDETAILS               1002
 
@@ -11,6 +10,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1003
